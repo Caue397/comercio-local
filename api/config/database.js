d28ad013aec64848bd509cfa8f84f96f288582ee
Cauê Gallizzi <cauegallizzi397@gmail.com>
2024-03-11 10:42:10 -0300

    update database config

--- api/config/database.js
+++ api/config/database.js
@@ -5,7 +5,7 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.DB_URI).then(
-    () => console.loh('Conectado ao MongoDB')
+    () => console.log('Conectado ao MongoDB')
   ).catch(
     (err) => console.log(`Erro ao conectar com MongoDB: ${err}`)
   )
