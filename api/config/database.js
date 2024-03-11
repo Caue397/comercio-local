c572e726ccff5fac81d8aa7550bafbfe58f26cd2
Cauê Gallizzi <cauegallizzi397@gmail.com>
2024-03-11 10:37:15 -0300

    update

--- api/config/database.js
+++ api/config/database.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.DB_URI);
-  console.log('Conectado ao MongoDB')
+  await mongoose.connect(process.env.DB_URI).then(
+    () => console.loh('Conectado ao MongoDB')
+  ).catch(
+    (err) => console.log(`Erro ao conectar com MongoDB: ${err}`)
+  )
 }
