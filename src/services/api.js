29a748f64dc0a4fd2948f659eade4eca69e30da3
Cauê Gallizzi <cauegallizzi397@gmail.com>
2024-07-10 14:54:28 -0300

    update service

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://comercio-local.onrender.com/'
+    baseURL: 'https://comercio-local-vr1h.vercel.app/'
 })
 
 export default api
